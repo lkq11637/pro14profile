aa61aa23b61c2c1cf4a9c0f67eabf7caffdcdf8b
tom <1163768006@qq.com>
2022-07-09 17:52:25 +0800

    pc2 commit11

--- src/main/java/com/atguigu/maven/ProfileTest.java
+++ src/main/java/com/atguigu/maven/ProfileTest.java
@@ -3,33 +3,7 @@ package com.atguigu.maven;
 public class ProfileTest {
 
     public static void main(String[] args) {
-
-        new Thread(()->{
-            System.out.println(Thread.currentThread().getName() + " is working");
-        });
-
         System.out.println("1234");
-        System.out.println("master commit");
-        System.out.println("hot-fix");
-
-        System.out.println("asdfasdf");
-        System.out.println("asdfasdf");
-        System.out.println("asdfasdf");
-        System.out.println("git test");
-        System.out.println("git test123");
-        System.out.println("不同的位置修改");
-        System.out.println("123");
-        System.out.println("pc2");
-        System.out.println("pc222222222");
-        System.out.println("pc1");
-        System.out.println("pc1");
-        System.out.println("pc1");
-
-        System.out.println("feach12345555");
-        System.out.println("feach222222222");
-        System.out.println("feach444444444");
-        System.out.println("feach555");
-        System.out.println("434");
     }
 }
 
