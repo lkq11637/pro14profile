3066a789cfbc14cc3d6be378d8b2bfbfb8ffa9f3
tom <1163768006@qq.com>
2022-07-09 18:18:42 +0800

    pc2 commit11

--- src/main/java/com/atguigu/maven/ProfileTest.java
+++ src/main/java/com/atguigu/maven/ProfileTest.java
@@ -3,12 +3,7 @@ package com.atguigu.maven;
 public class ProfileTest {
 
     public static void main(String[] args) {
-        System.out.println("2345");
-        System.out.println("1234");
-        System.out.println("2234");
-        System.out.println("525");
-        System.out.println("525");
-        System.out.println("1234");
+        System.out.println("abcd");
     }
 }
 
