b6a7fce5b56dc00520cc6560bf329f2669c6acef
tom <1163768006@qq.com>
2022-07-09 18:05:31 +0800

    123

--- src/main/java/com/atguigu/maven/ProfileTest.java
+++ src/main/java/com/atguigu/maven/ProfileTest.java
@@ -5,6 +5,7 @@ public class ProfileTest {
     public static void main(String[] args) {
         System.out.println("2345");
         System.out.println("1234");
+        System.out.println("2234");
     }
 }
 
